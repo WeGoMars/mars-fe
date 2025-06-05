b8965530ef591d6f5143cd3d0ddbcb2b5a03873e
Jun-Lae Kim <juntop223@gmail.com>
2025-06-05 15:23:53 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css";
 import { store } from "@/lib/store"; //
 import { Provider } from "react-redux";
-;
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
